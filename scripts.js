b74fed69f6ca1268da4cf196f505c47b0baf712d
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-17 14:46:08 -0800

    add time ending to display

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer (seconds) {
   const now = Date.now();
   const then = now + (seconds * 1000);
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   setInterval(() => {
       const secondsLeft = Math.round((then - Date.now())/1000);
@@ -24,4 +26,11 @@ function displayTimeLeft(seconds){
   timerDisplay.textContent = display;
   document.title = display;
   console.log({minutes, remainderSeconds});
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${hour}:${minutes}`;
 }
