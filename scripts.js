7f08d701d2de3d0cdf95cc7c3377c65789b98fd4
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-17 15:43:08 -0800

    fixed timer to only take last entry

--- scripts.js
+++ scripts.js
@@ -3,17 +3,17 @@ const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
-function timer (seconds) {
+function timer(seconds) {
   // clear any existing timers
-
+  clearInterval(countdown);
 
   const now = Date.now();
   const then = now + (seconds * 1000);
   displayTimeLeft(seconds);
   displayEndTime(then);
 
-  setInterval(() => {
-      const secondsLeft = Math.round((then - Date.now())/1000);
+  countdown = setInterval(() => {
+      const secondsLeft = Math.round((then - Date.now()) / 1000);
       if(secondsLeft < 0) {
         clearInterval(countdown);
         return;
@@ -26,7 +26,7 @@ function timer (seconds) {
 function displayTimeLeft(seconds){
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds - (minutes * 60);
-  const display = `${minutes}:${remainderSeconds < 10 ? '0' : "" }${remainderSeconds}`;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
   timerDisplay.textContent = display;
   document.title = display;
   console.log({minutes, remainderSeconds});
@@ -37,7 +37,7 @@ function displayEndTime(timestamp) {
   const hour = end.getHours();
   const adjustedHour = hour > 12 ? hour - 12 : hour;
   const minutes = end.getMinutes();
-  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ""}${minutes}`;
+  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
 
 function startTimer() {
