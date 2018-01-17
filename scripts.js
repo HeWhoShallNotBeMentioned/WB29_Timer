c918bca123ccfa81116fdaf8ec4cf65c8e601414
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-17 15:32:25 -0800

    add code from grabbing text from the form

--- scripts.js
+++ scripts.js
@@ -4,6 +4,9 @@ const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer (seconds) {
+  // clear any existing timers
+
+
   const now = Date.now();
   const then = now + (seconds * 1000);
   displayTimeLeft(seconds);
@@ -43,3 +46,10 @@ function startTimer() {
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  console.log(mins);
+  timer(mins * 60);
+  this.reset();
+});
